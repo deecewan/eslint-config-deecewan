e94e7ff4537102f5669e485bbe4945cf0da42ca0
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-10-29 23:15:46 +1000

    enable eslint-plugin-eslint-comments

--- packages/base/index.js
+++ packages/base/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
-  plugins: ['jest', 'prettier'],
+  plugins: ['jest', 'prettier', 'eslint-comments'],
   env: {
     'jest/globals': true,
   },
@@ -17,5 +17,15 @@ module.exports = {
     'jest/no-identical-title': 2,
     /* ensure that `expect` has been called correctly */
     'jest/valid-expect': 2,
+    /* if you disable an eslint rule, you must re-enable it */
+    'eslint-comments/disable-enable-pair': 2,
+    /* duplicate disables is probably a sign of a mistake */
+    'eslint-comments/no-duplicate-disable': 2,
+    /* if you're disabling a line, you need to specify which rule explicitly */
+    'eslint-comments/no-unlimited-disable': 2,
+    /* you should not keep lines disabled when they have no errors */
+    'eslint-comments/no-unused-disable': 2,
+    /* sanity check to ensure you're re-enabling the correct rules */
+    'eslint-comments/no-unused-enable': 2,
   },
 };
